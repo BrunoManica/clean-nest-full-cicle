e8dd08036258398165ffe60c937bce63e05e68ac
Bruno Manica <bbmanica@gmail.com>
2023-12-11 17:33:08 -0300

    los ajustes

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { List } from './entities/list.entity';
@@ -5,7 +6,12 @@ import { ListsController } from './lists.controller';
 import { ListsService } from './lists.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([List])],
+  imports: [
+    SequelizeModule.forFeature([List]),
+    HttpModule.register({
+      baseURL: 'http://localhost:6000',
+    }),
+  ],
   controllers: [ListsController],
   providers: [ListsService],
 })
--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,15 +1,22 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { lastValueFrom } from 'rxjs';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 import { List } from './entities/list.entity';
 
 @Injectable()
 export class ListsService {
-  constructor(@InjectModel(List) private listModel: typeof List) {}
+  constructor(
+    @InjectModel(List) private listModel: typeof List,
+    private httpService: HttpService,
+  ) {}
 
-  create(createListDto: CreateListDto) {
-    return this.listModel.create(createListDto);
+  async create(createListDto: CreateListDto) {
+    const list = await this.listModel.create(createListDto);
+    await lastValueFrom(this.httpService.post('list', { name: list.name }));
+    return list;
   }
 
   findAll() {
